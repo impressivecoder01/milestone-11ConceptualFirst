c2222045023415b2aefd52533e2f41e5eb1385ef
impressivecoder01 <rohanrahman251@gmail.com>
2024-12-18 02:03:50 +0600

    get a single data from db by id

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ async function run() {
       const result = await jobsCollection.deleteOne(query)
       res.send(result)
     })
+    // get a single job data by id from db
+    app.get('/job/:id',async(req,res) => {
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
